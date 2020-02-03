17ff3a3b1f4828fbb3bd521b3a79a22e8af6df0e
Egor Syrtcov <sev_84@inbox.ru>
2020-02-03 13:06:30 +0300

    Добавление жестов и новый переход по долгому нажатию на картинку

---
+++ Work with UIGestureRecognizer and Animating/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  Work with UIGestureRecognizer and Animating
+//
+//  Created by Egor Syrtcov on 2/3/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+ final class DetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Work with UIGestureRecognizer and Animating/ViewController.swift
+++ Work with UIGestureRecognizer and Animating/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var count = 0
+    var count = 0 {
+        didSet {
+            mainLabel.text = "\(count)"
+        }
+    }
     
     @IBOutlet weak var mainLabel: UILabel!
     @IBOutlet weak var mainImageView: UIImageView?
-    
+    @IBOutlet weak var gestureLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupImageGesture()
+        setupSwipeRecognizer()
     }
     
     private func setupImageGesture() {
@@ -28,10 +33,47 @@ class ViewController: UIViewController {
     }
   
     @objc func onTapOneImage() {
-        mainLabel.text = "Нажали: \(count)"
         count+=1
     }
-  
+    
+   private func setupSwipeRecognizer() {
+    let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestRecognizer))
+    swipeRight.direction = UISwipeGestureRecognizer.Direction.right
+    mainImageView?.addGestureRecognizer(swipeRight)
+    
+    let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipeGestRecognizer))
+    swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
+    mainImageView?.addGestureRecognizer(swipeLeft)
+    
+    let longPress = UILongPressGestureRecognizer(target: self, action: #selector(self.longPressed))
+    mainImageView?.addGestureRecognizer(longPress)
+    
+    }
+    
+    
+    @IBAction func swipeGestRecognizer(_ sender: UISwipeGestureRecognizer) {
+        
+        switch sender.direction {
+        case .left:
+            gestureLabel.text = "Сделан swipe влево"
+            print("Сделан swipe влево")
+        case .right:
+            gestureLabel.text = "Сделан swipe вправо"
+            print("Сделан swipe вправо")
+        default:
+            print("Error")
+        }
+    }
+    
+    
+    @IBAction func longPressed(_ sender: UILongPressGestureRecognizer) {
+        
+        if sender.state == .began {
+            let controller = UIStoryboard(name: "DetailViewController", bundle: nil).instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+            navigationController?.pushViewController(controller, animated: true)
+        }
+    }
+    
     @IBAction func buttonNextVC(_ sender: Any) {
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
         let newController = storyBoard.instantiateViewController(withIdentifier: "secondVC") as! SecondViewController
