c2ac022c41ad5decb4790edc15a6d7acf7f2e8be
Egor Syrtcov <sev_84@inbox.ru>
2020-02-04 14:16:19 +0300

    Создание ViewController с кнопками перехода

---
+++ Work with UIGestureRecognizer and Animating/Screens/Transition Screen.swift
@@ -0,0 +1,17 @@
+//
+//  Transition Screen.swift
+//  Work with UIGestureRecognizer and Animating
+//
+//  Created by Egor Syrtcov on 2/4/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+class TransitionScreens: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
