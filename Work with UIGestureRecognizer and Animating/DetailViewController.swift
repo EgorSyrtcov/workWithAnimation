3e09b17716a09f2dc51b44c61042f2894bd461b5
Radomyr Sidenko <radik-sidehnko@ukr.net>
2020-02-04 11:01:36 +0200

    пример использования hitTest

--- Work with UIGestureRecognizer and Animating/DetailViewController.swift
+++ Work with UIGestureRecognizer and Animating/DetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
- final class DetailViewController: UIViewController {
+class DetailViewController: UIViewController {
     
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
@@ -19,16 +19,31 @@ import UIKit
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
+//        super.viewDidLoad()
+//        button2.becomeFirstResponder()
+        let gesture:UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                    action: #selector(tapView))
+        view.addGestureRecognizer(gesture)
+//        button2.resignFirstResponder()
     }
     
-    @IBAction func tapButton1(_ sender: Any) {
+    @objc func tapView() {
+        view.backgroundColor = .green
+    }
+    
+    @IBAction func tapButton1(_ sender: UIButton) {
         print("Сработала кнопка 1")
+        view.backgroundColor = sender.backgroundColor
     }
     
     @IBAction func tapButton2(_ sender: UIButton) {
          print("Сработала кнопка 2")
+        view.backgroundColor = sender.backgroundColor
+    }
+    
+    
+    func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        return nil
     }
     
     @IBAction func longTappedGesture(_ sender: UILongPressGestureRecognizer) {
@@ -68,6 +83,26 @@ import UIKit
         if !shouldTakeAction { return }
       
        
+    }
+    
+}
+
+
+extension UIButton {
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("first View touch started", touches.first?.location(in: self))
+        
+        (touches.first?.view as? UIButton)?.resignFirstResponder()
+        super.touchesBegan(touches, with: event)
+    }
+    
+    override open func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        
+        let test = CGRect(origin: point, size: CGSize(width: 10, height: 10))
+        if test.contains(CGPoint(x: 11, y: 42)) {
+            return nil
+        }
+        return super.hitTest(point, with: event)
     }
     
 }
